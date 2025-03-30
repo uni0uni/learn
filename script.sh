2852078bc0e84c3c7145689d836da421eddc4e07
uni <uni0ni0uni@gmail.com>
2025-03-30 07:32:53 -0700

    1

--- script.sh
+++ script.sh
@@ -1,1 +1,6 @@
-echo "TODO: add the script";
+apt-get update -y && \
+apt-get upgrade -y && \
+apt-get install build-essential -y && \
+apt-get install bear -y && \
+apt-get instal curl -y && \
+snap install code --classic
